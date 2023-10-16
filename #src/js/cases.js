87b33a9604329671dd5919a8deb38dbb80aeca6a
Dunya <dunya2928@mail.ru>
2023-10-16 13:27:28 +0300

    chenge url del /proekty/

--- #src/js/cases.js
+++ #src/js/cases.js
@@ -1,56 +1,45 @@
 "use strict"
-window.addEventListener("load", () => {
-  if (document.querySelector(".works-card")) {
-    document.querySelectorAll(".works-card").forEach(item => {
-    if ( item.getAttribute("href").includes(sessionStorage.getItem("projectPath"))) {
-      setTimeout(() => {
-        sessionStorage.removeItem("projectPath")
-      }, 200);
-      item.click()
-      return
-    }
-    })
+document.addEventListener("DOMContentLoaded", ()=> {
+  let urlArr = location.href.split("/")
+  let idx = urlArr.findIndex(i => "proekty" === i)
+  if (idx != -1 && (idx != (urlArr.length - 1)) && urlArr[idx + 1] != "") {
+    sessionStorage.setItem("projectPath", location.pathname)
+    let url = urlArr.slice(0, urlArr.length - 1).join("/")
+    window.location.replace(
+      url,
+    );
   }
 })
-$(document).on('click', '.modal .js-pageup', function () {
-  $('.modal__overlay').stop().animate(
-    {
-      scrollTop: 0, 
-    },
-    1500
-  );
-});
 let i = 0
 function loaded(item) {
-    item.src = item.dataset.src
-    if (item.parentNode.querySelector("source")) {
-      item.parentNode.querySelectorAll("source").forEach(el => {
-        el.srcset = el.dataset.srcset
-      })
-    }
+  item.src = item.dataset.src
+  if (item.parentNode.querySelector("source")) {
+    item.parentNode.querySelectorAll("source").forEach(el => {
+      el.srcset = el.dataset.srcset
+    })
+  }
 }
 function loadImg(lazyImages) {
-    loaded(lazyImages[i])
-    document.querySelectorAll(".lazy")[i].onload = function nextImg() {
-      lazyImages[i].removeAttribute("data-src")
-      if (lazyImages[i].parentNode.querySelector("source")) {
-        lazyImages[i].parentNode.querySelectorAll("source").forEach(el => {
-          el.removeAttribute('data-srcset')
-        })
-      }
-      if (i === lazyImages.length - 1) {
-        document.querySelector(".case__body").style.pointerEvents = "auto"
-      }
-      if (i < lazyImages.length - 1){
-        i++
-        loadImg(lazyImages)
-      }
+  loaded(lazyImages[i])
+  document.querySelectorAll(".lazy")[i].onload = function nextImg() {
+    lazyImages[i].removeAttribute("data-src")
+    if (lazyImages[i].parentNode.querySelector("source")) {
+      lazyImages[i].parentNode.querySelectorAll("source").forEach(el => {
+        el.removeAttribute('data-srcset')
+      })
     }
+    if (i === lazyImages.length - 1) {
+      document.querySelector(".case__body").style.pointerEvents = "auto"
+    }
+    if (i < lazyImages.length - 1) {
+      i++
+      loadImg(lazyImages)
+    }
+  }
 }
-
 if (document.querySelectorAll(".lazy").length > 0) {
   let lazyImages = document.querySelectorAll(".lazy")
-   loadImg(lazyImages) 
+  loadImg(lazyImages)
 }
 function enableScroll() {
   if (document.querySelectorAll('.fixed-block')) {
@@ -79,38 +68,10 @@ function openModal(modal) {
 function closeModal(modal) {
   modal.classList.remove("open")
   setTimeout(() => {
-      enableScroll()
-  },300);
+    enableScroll()
+  }, 300);
 }
-/* document.querySelector(".case__body").addEventListener("click", ()=> {
-    document.querySelector(".modal__inner").innerHTML = document.querySelector(".case__body").innerHTML
-    setTimeout(() => {
-      openModal(document.querySelector(".fancy-modal"))
-    }, 0);
-}) */
-/* document.querySelector(".modal__overlay").addEventListener("click", e => {
-  if (!document.querySelector(".modal__content").contains(e.target)) {
-    closeModal(document.querySelector(".fancy-modal"))
-  }
-}) */
 
-let f = location.href.split("/")
-let e = f.findIndex(t => "proekty" === t)
-if (e != -1 && (e != (f.length - 1)) && f[e+1] != "") {
-  sessionStorage.setItem("projectPath",location.pathname)
-  let url = f.slice(0, f.length - 1).join("/")
-  window.location.replace(
-  url ,
-  );
-
-} 
-document.querySelector(".modal__overlay").addEventListener("scroll", () => {
-  if (document.querySelector(".modal__overlay").scrollTop > 400) {
-    document.querySelector(".js-pageup").classList.add("show")
-  } else {
-    document.querySelector(".js-pageup").classList.remove("show")
-  }
-})
 function changeUrl(item) {
   let title
   let url
@@ -120,8 +81,6 @@ function changeUrl(item) {
   }
   function fetchPage(item) {
     let href = item.getAttribute("href")
-    //$( ".proj__cards" ).load( "swiss.html .case__body" );
-    console.log(href)
     fetch(href)
       .then(response => {
         if (response.ok) {
@@ -133,54 +92,83 @@ function changeUrl(item) {
         let doc = (new window.DOMParser()).parseFromString(res, "text/html")
         document.title = doc.title
         document.querySelector('.fancy-modal .modal__inner').innerHTML = doc.querySelector(".case__content").innerHTML
-       }) 
-       .then(res => {
+      })
+      .then(res => {
         openModal(document.querySelector('.fancy-modal'))
         window.history.pushState("", "", href)
-        let modal = document.querySelector(".fancy-modal") 
+        let modal = document.querySelector(".fancy-modal")
         if (modal && modal.querySelector(".works-card")) {
           modal.querySelectorAll(".works-card").forEach(item => {
             item.addEventListener("click", e => {
               e.preventDefault()
               $('.modal__overlay').stop().animate(
                 {
-                  scrollTop: 0, 
+                  scrollTop: 0,
                 },
                 1500
               );
               fetchPage(item)
             })
           })
-          
+
         }
         if (modal && modal.querySelectorAll(".lazy").length > 0) {
           i = 0
           let lazyImages = modal.querySelectorAll(".lazy")
-           loadImg(lazyImages) 
+          loadImg(lazyImages)
         }
         document.querySelector(".modal__overlay").addEventListener("click", e => {
           if (!document.querySelector(".modal__content").contains(e.target)) {
             document.title = title
-            console.log(history)
             window.history.replaceState("", "", url)
             //window.history.back()
-            closeModal(document.querySelector(".fancy-modal"))            
+            closeModal(document.querySelector(".fancy-modal"))
           }
         })
-      
-       }) 
+
+      })
       .catch((error) => {
         console.log(error)
       });
   }
   fetchPage(item)
 }
-if (document.querySelector(".works-card")) {
-  document.querySelectorAll(".works-card").forEach(item => {
-    item.addEventListener("click", e => {
-      e.preventDefault()
-      changeUrl(item)
+window.addEventListener("load", () => {
+  if (document.querySelector(".works-card")) {
+    document.querySelectorAll(".works-card").forEach(item => {
+      if (item.getAttribute("href").includes(sessionStorage.getItem("projectPath"))) {
+        setTimeout(() => {
+          sessionStorage.removeItem("projectPath")
+        }, 200);
+        item.click()
+        return
+      }
     })
-  })
+  }
+  $(document).on('click', '.modal .js-pageup', function () {
+    $('.modal__overlay').stop().animate(
+      {
+        scrollTop: 0,
+      },
+      1500
+    );
+  });
+  if (document.querySelector(".modal__overlay")) {
+    document.querySelector(".modal__overlay").addEventListener("scroll", () => {
+      if (document.querySelector(".modal__overlay").scrollTop > 400) {
+        document.querySelector(".js-pageup").classList.add("show")
+      } else {
+        document.querySelector(".js-pageup").classList.remove("show")
+      }
+    })
+  }
+  if (document.querySelector(".works-card")) {
+    document.querySelectorAll(".works-card").forEach(item => {
+      item.addEventListener("click", e => {
+        e.preventDefault()
+        changeUrl(item)
+      })
+    })
   
-}
+  }
+})
